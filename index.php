e94cb1a9225188f41caed046db80d79fca440e32
Jozef W <62598812+Jozefwl@users.noreply.github.com>
2022-11-16 22:45:40 +0100

    index page and javascript code
    Added a div into the main page, getting closer to final version.
    
    Added comments to javascript file because of learning, and in case I want to revise the code later i have something to refer to.
    
    Added animations and a bunch of other stuff into the css file.

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
-
+<head>
 	<title>Main Page - JW</title> 
 	<link rel="stylesheet" href="cascade.css">
-
-	</style>
+	<script src="typewriter.js"></script> 
+	<script src="linkedin.js"></script> 
 </head>
-<script src="typewriter.js"></script> 
-<body onload="typeWriter()">
 
-<div class="bluebox">
+	<body onload="typeWriter()">
+
+	<div class="bluebox">
 <p id="typing"></p>		
-</div>
+	</div>
 
-<div class="selectionWrapper">
-	<div class="selection"><a href="">HOME</a></div>
-	<div class="selection"><a href="">ABOUT</a></div>
-	<div class="selection"><a href="">CONTENTS</a></div>
-	<div class="selection"><a href="">CONTACTS</a></div>
-</div>
+	<div class="selectionWrapper">
+<a href="">	<div class="selection">HOME</div>		</a>
+<a href="">	<div class="selection">ABOUT</div>		</a>
+<a href="">	<div class="selection">CONTENTS</div>	</a>
+<a href="">	<div class="selection">CONTACTS</div>	</a>
+	</div>
+	<!-- <div class="greybox"><img src="images/jozef.png" width="40%" height="40%"></div> --> 
 
 <div class="mainbox">
-	<div class="mainbox_blue">
-<div class="mainbox_grey">
+	<a href="https://www.linkedin.com/in/jozef-waldhauser-337779220/" id="title"><h1>Jozef Waldhauser</h1></a>
+
+	<p id="title">Hello! My name is <a href="https://www.linkedin.com/in/jozef-waldhauser-337779220/" id="title">Jozef Waldhauser</a>, and this is my website, you can learn more info about me by clicking my name.</p>
+	<p id="title"></p>
+	<iframe id="linkedin" src="https://www.linkedin.com/embed/feed/update/urn:li:share:6945672564824309760" allowfullscreen="" width="500" height="100%" title="Embedded post" frameborder="0"></iframe>
+		  
+	
+</div>
 
 
-<P ALIGN="CENTER">
-CURRENTLY UNDER CONSTRUCTION!!
-</p>
-</div>
-</div>
-</div>
 
 </body>
 </html>
--- typewriter.js
+++ typewriter.js
@@ -1,32 +1,67 @@
-//copied from w3schools
-
-//https://www.w3schools.com/howto/howto_js_typewriter.asp
-
 var i = 0;
-var txt = '.waldhauser_';
-var speed = 80;
+var text = '.waldhauser_'; //string to 'write'
+var speed = 80; //delay in ms
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typing").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+/* ----------------------
+
+  Because JS is an asynchronous language, meaning code execution is whatever comes first,
+  meaning a going from top-bottom, whichever finishes first ex. a function to fetch data followed by a 
+  text to console output: the console output is finished first, but the fetching takes time and thereafter
+  is output later, even if the call for it was before the text console output
+
+  https://www.sitepoint.com/delay-sleep-pause-wait/
+
+   ---------------------- */ 
+
+function sleep() {
+  return new Promise(resolve => { 
+    setTimeout(() => {            
+      resolve();                  
+    }, speed);
+  });
+}
+  
+/* ---------------------------------------
+
+-> return new Promise(resolve => { 
+
+the promise object represents the completion of an async operation and resulting value
+the simplest explanation: i promise to do this() then you can do that()
+what are promises: https://javascript.info/promise-basics
+using promises: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+
+
+
+-> setTimeout(() => { 
+
+setTimeout executes a function after a set delay,
+what are arrow functions: https://javascript.info/arrow-functions-basics
+
+
+  ---------------------------------------  */
+
+async function typeWriter(){ //async function https://javascript.info/async-await
+  for(i=0; i<text.length; i++){ //iterates through entire string length
+    document.getElementById("typing").innerHTML += text.charAt(i); //places character from string's i position
+    await sleep();
   }
 }
-/* start of comment
 
-function typingWriter(){ //fucntion takes string text and HTML element id
-    
-  for(i=0;i < string.length;i++){ //loops for the entire length of the string
-  document.getElementById("typing").innerHTML += string.charAt(i); //adds the character
-  setTimeout(typeWriter, speed);
+
+/*
+
+W3SCHOOLS ORIGINAL CODE
+
+
+function typeWriter() { //writes to elementId like a 'typewriter'
+  if (i < txt.length) { 
+    document.getElementById("typing").innerHTML += txt.charAt(i); //places character from string almost array-like
+    i++; //increments i to stop the function after all characters have been 'written'
+    setTimeout(typeWriter, speed); //calls the fuction typeWriter after a delay 'speed'
   }
 }
 
+This function uses the setTimeout function, which calls for a function (executes it) after a specified interval (speed)
+so it needs an if statement because the function always starts running from the start when it gets to the setTimeout()
 
-
-end of comment */
-
-
-
-//still dunno why forloop doesnt work
+*/
